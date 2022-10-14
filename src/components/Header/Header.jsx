2ffc516659548c1c0964c7564ef404a60f92f74b
Phats Lys <lhpworking@gamil.com>
2022-10-14 23:23:26 +0700

    update logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import "./Header.scss"
+import "./Header.scss";
 
 export default function Header() {
   return (
     <header className="header">
       <div className="container">
         <a className="logo">
-          <img src="/src/assets/react.svg" alt="logo" /> NASA
+          <img src="/assets/react.svg" alt="logo" /> NASA
         </a>
       </div>
     </header>
